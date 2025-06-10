65a6f54675573edb4321116c186d9546e4f34eb3
Adri-Lopes <adri.lopes@live.com>
2025-06-10 20:51:26 -0300

    ajuste

---
+++ server/middleware/cors.global.ts
@@ -0,0 +1,27 @@
+import { defineEventHandler, getHeader, getMethod, setResponseHeaders } from 'h3'
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://prontuario-medguias.meuprojetoweb.com.br'
+]
+
+export default defineEventHandler(async (event) => {
+  const origin = getHeader(event, 'origin')
+
+  if (origin && allowedOrigins.includes(origin)) {
+    setResponseHeaders(event, {
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Max-Age': '86400'
+    })
+  }
+
+  // Handle preflight OPTIONS request
+  if (getMethod(event) === 'OPTIONS') {
+    event.node.res.statusCode = 204
+    event.node.res.end()
+    return
+  }
+})
