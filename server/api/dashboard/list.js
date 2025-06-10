ae53b5e66458d1ee17e420fa600e21e63a730e18
Adri-Lopes <adri.lopes@live.com>
2025-06-10 20:32:22 -0300

    dash

--- server/api/dashboard/list.js
+++ server/api/dashboard/list.js
@@ -2,12 +2,9 @@ export default defineEventHandler(() => {
   return {
     success: true,
     message: "Dashboard carregado com sucesso.",
-    data: [
-      {
-        patients: 4,
-        insurance: 3,
-      }
-
-    ],
+    data: {
+      patients: 4,
+      insurance: 3,
+    },
   };
 });
